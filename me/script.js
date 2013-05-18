b56813196ae4572cb69e96121e1c13470d15fdf9
jdev005 <jdev005@gmail.com>
2013-05-18 22:28:58 +0530

    fixed preloader

--- me/script.js
+++ me/script.js
@@ -40,3 +40,8 @@ $('#up').addClass('move-up');
 },3000)
 
 });
+
+$(window).load(function() { // makes sure the whole site is loaded
+            $("#status").fadeOut(); // will first fade out the loading animation
+            $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
+        })

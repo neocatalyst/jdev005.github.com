e219ec5b4f5ff9f3e1bd4d1eecd52e99bcd38040
jdev005 <jdev005@gmail.com>
2013-05-18 10:00:48 +0530

    added page trasitions

--- me/script.js
+++ me/script.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+	$("body").css("display", "none");
+ 
+    $("body").fadeIn(2000);
+ 
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1500, redirectPage);     
+    });
+         
+    function redirectPage() {
+        window.location = linkLocation;
+    }
 	$('#nav').on("click",function(){
 		$('#navigation').toggle("slow");
 	});
